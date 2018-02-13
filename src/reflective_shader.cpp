3c36c8de9a38646ae734f1badf808cfe69f412a5
I-HSIEN HUANG <isytkr34@gmail.com>
2018-02-12 16:58:12 -0800

    add reflective ray and calculate the color

--- src/reflective_shader.cpp
+++ src/reflective_shader.cpp
@@ -8,5 +8,21 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     // TODO: determine the color
+    /*
+    Noramlly R = 2*(LN)*N-L // L is light
+    However, here we need to count by the ray
+    color will add orignal color and the closest_intersection hitted by the reflection Ray
+    Keep reflection
+
+    */
+         vec3 CR;
+         vec3 C0;
+         vec3 R;
+         vec3 RD = (-1.0)*ray.direction;
+         R = 2.0*dot(RD.normalized(),same_side_normal)*same_side_normal-RD.normalized();
+         Ray New_Ray(intersection_point,R);
+         CR = world.Cast_Ray(New_Ray,++recursion_depth);
+         C0 = this->shader->Shade_Surface(ray,intersection_point,same_side_normal,recursion_depth,is_exiting);
+         color = C0 + reflectivity*(CR-C0); 
     return color;
 }
