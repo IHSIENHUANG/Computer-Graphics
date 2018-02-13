2c7dc6dcce97f174b31c3b49bb7412d2bebb6c63
I-HSIEN HUANG <isytkr34@gmail.com>
2018-02-12 17:00:30 -0800

    add new sphere intersection judgement

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -1,6 +1,6 @@
+
 #include "sphere.h"
 #include "ray.h"
-
 
 // Determine if the ray intersects with the sphere
 bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
@@ -13,7 +13,8 @@ bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
     // c = (e-c) * (e-c) -r*r: double
     // t2 > t1
     double a = dot(ray.direction, ray.direction);
-    double b = 2.0* dot((ray.direction), (ray.endpoint - center));
+   // double a = 1.0;
+    double b = (2.0)*dot((ray.direction), (ray.endpoint - center));
     double c = dot((ray.endpoint - center) , (ray.endpoint - center))-radius*radius;
     double t1,t2;
     double decision = b*b-4.0*a*c;
@@ -22,31 +23,52 @@ bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
 	 return false;
     else if( decision== 0)
     {
-	/*t1= ((-1.0)*(b)+sqrt(decision))/(2.0*a);
-        new_hit1.t = t1;
-	if(t1<0)
-        	new_hit1.ray_exiting= true;
-        hits.push_back(new_hit1);*/
-	return false; 	
+    	return false; 	
     }
     else
     {
+        
 	t1= ((-1.0)*(b)-sqrt(decision))/(2.0*a);
 
         new_hit1.t = t1;
-	new_hit1.ray_exiting = false;
+	    new_hit1.ray_exiting = false;
 	t2= ((-1.0)*(b)+sqrt(decision))/(2.0*a);
-        new_hit1.t = t2;
+        new_hit2.t = t2;
         new_hit2.ray_exiting = true;
 	new_hit1.object=this;
 	new_hit2.object=this;
-    //make sure t2 is inserted before t1 if t1 is existing 
+    //make sure t2 is inserted before t1 if t1 is existing
+    
+    if(t2>0)
+    {
+        if(t1>0)
+        {
+           hits.push_back(new_hit1);
+           hits.push_back(new_hit2);
+           return true; 
+        }    
+        else
+        {
+           new_hit1.t=0;
+           hits.push_back(new_hit1);
+           hits.push_back(new_hit2);
+            return true;
+        
+        }
+        printf("wtf\n");
+        return false;
+    }
+    else
+        return false;
+     /*    
 	if(t2>0)
 		hits.push_back(new_hit2);
     if(t1>0)//if t1 is exiting it should not be viewed as hit
         hits.push_back(new_hit1);
-
+    if(t1<=0 && t2<=0)
+        return false;
 	return true;
+    */
     }   
 }
 
@@ -54,6 +76,7 @@ vec3 Sphere::Normal(const vec3& point) const
 {
     vec3 normal;
     // TODO: set the normal
-    normal = point.normalized();
+    normal = point - center;
+    normal = normal.normalized();
     return normal;
 }
